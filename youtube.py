dd465b3a0bd5eb17a1546f9421f120a5bee9b377
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2021-04-15 08:07:19 +0530

    Add files via upload

--- youtube.py
+++ youtube.py
@@ -1,29 +1,29 @@
 import pytube
 
 def you(link):
-    yt = pytube.YouTube(link)
-    print(yt)
-    print(yt.title)
-    stream = yt.streams.filter(abr="50kbps", type="audio")[0]
-    stream1=yt.streams.filter(res="144p")[0]
-    print(stream)
-    print(stream1)
-    print(stream.download("youtube","temp"))
-    print(stream.download("youtube","temp1"))
-    combine_audio('./youtube/temp.webm', './youtube/temp1.mp4', './youtube/temp2.mp4')
-    return "temp2"
+	yt = pytube.YouTube(link)
+	print(yt)
+	print(yt.title)
+	stream = yt.streams.filter(abr="50kbps", type="audio")[0]
+	stream1=yt.streams.filter(res="144p")[0]
+	print(stream)
+	print(stream1)
+	print(stream.download("youtube","temp"))
+	print(stream.download("youtube","temp1"))
+	combine_audio('./youtube/temp.webm', './youtube/temp1.mp4', './youtube/temp2.mp4')
+	return "temp2"
 
 def combine_audio(inpaud, inpvid, outp):
-    
-    import ffmpeg
+	
+	import ffmpeg
 
-    input_video = ffmpeg.input(inpvid)
+	input_video = ffmpeg.input(inpvid)
 
-    input_audio = ffmpeg.input(inpaud)
+	input_audio = ffmpeg.input(inpaud)
 
-    ffmpeg.concat(input_video, input_audio, v=1, a=1).output(outp).run()
+	ffmpeg.concat(input_video, input_audio, v=1, a=1).output(outp).run()
 
 if __name__=="__main__":
-    
-    you("https://youtu.be/KS6XeRkN_us")
-    
+	
+	you("https://youtu.be/KS6XeRkN_us")
+	__main__
