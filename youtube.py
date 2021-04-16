77538be476315b151d3ca992bc558404ce5a9829
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2021-04-16 20:56:40 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -4,31 +4,24 @@ def you(link):
 	yt = pytube.YouTube(link)
 	print(yt)
 	print(yt.title)
-	#stream = yt.streams.filter(abr="50kbps", type="audio")[0]
+	stream = yt.streams.filter(abr="50kbps", type="audio")[0]
 	stream1=yt.streams.filter(res="144p")[0]
-	#print(stream)
+	print(stream)
 	print(stream1)
-	#print(stream.download("youtube","temp"))
-	print(stream.download("youtube","temp1"))
-	#combine_audio('./youtube/temp.webm', './youtube/temp1.mp4', './youtube/temp2.mp4')
+	print(stream.download("youtube","temp"))
+	print(stream1.download("youtube","temp1"))
+	combine_audio('./youtube/temp.webm', './youtube/temp1.mp4', './youtube/temp2.mp4')
 	return "temp1"
 
-def combine_audio(audname, vidname, outname, fps=25):
-    import moviepy.editor as mpe
-    my_clip = mpe.VideoFileClip(vidname)
-    audio_background = mpe.AudioFileClip(audname)
-    final_clip = my_clip.set_audio(audio_background)
-    final_clip.write_videofile(outname,fps=fps)
-
-#def combine_audio(inpaud, inpvid, outp):
+def combine_audio(inpaud, inpvid, outp):
 	
-	#import ffmpeg
+	import ffmpeg
 
-	#input_video = ffmpeg.input(inpvid)
+	input_video = ffmpeg.input(inpvid)
 
-	#input_audio = ffmpeg.input(inpaud)
+	input_audio = ffmpeg.input(inpaud)
 
-	#ffmpeg.concat(input_video, input_audio, v=1, a=1).output(outp).run()
+	ffmpeg.concat(input_video, input_audio, v=1, a=1).output(outp).run()
 	
 	#cmd = 'ffmpeg -y -i '+inpaud+'  -r 30 -i '+inpvid+'  -filter:a aresample=async=1 -c:a flac -c:v copy '+outp
 	#subprocess.call(cmd, shell=True)
