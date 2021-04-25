dd96f6a35feaab1e2963ace5d9d9342bdc2b42b2
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2021-04-25 19:43:17 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -15,18 +15,17 @@ def you(link):
 		shutil.copy('youtube/temp.webm', 'youtube/video.mp4')
 	except:
 		shutil.copy('youtube/temp.mp4', 'youtube/video.mp4')
-	#combine_audio('./youtube/temp.webm', './youtube/temp1.mp4', './youtube/temp2.mp4')
-	return True
+	combine_audio('./youtube/temp.webm', './youtube/temp1.mp4', './youtube/temp2.mp4')
+	return "temp2.mp4"
 
 def combine_audio(inpaud, inpvid, outp):
-	
-	import ffmpeg
-
-	input_video = ffmpeg.input(inpvid)
-
-	input_audio = ffmpeg.input(inpaud)
-
-	ffmpeg.concat(input_video, input_audio, v=1, a=1).output(outp).run()
+	#import ffmpeg
+	#input_video = ffmpeg.input(inpvid)
+	#input_audio = ffmpeg.input(inpaud)
+	#ffmpeg.concat(input_video, input_audio, v=1, a=1).output(outp).run()
+	import os
+	cmd = "ffmpeg -i "+inpvid+" -i "+inpaud+"^ -c copy "+outp
+	os.system(cmd)
 
 if __name__=="__main__":
 	
