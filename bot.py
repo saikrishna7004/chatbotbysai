42d634516fa397590eaff445519e1bf88d815dcb
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2021-04-25 19:47:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -252,12 +252,14 @@ def reply(message):
 		bot.send_message(chat, text1, reply_markup=keyboard)
 	elif "https://youtu" in text.lower() or "http://youtube.com" in text.lower() or "http://www.youtube.com" in text.lower():
 		import youtube
+		ret=youtube.you(text)
 		yn=bot.send_message(chat, "Trying to download...")
 		bot.edit_message_text("Download successful. Trying to upload...", chat, yn.id)
 		bot.send_document(chat, open("youtube/video.mp4", "rb"))
 		bot.send_document(chat, open("youtube/audio.mp3", "rb"))
 		open("youtube/video.mp4", "wb").close()
 		open("youtube/audio.mp3", "wb").close()
+		bot.send_document(chat, open("youtube/"+ret,"rb"))
 		bot.edit_message_text("Here is your video.", chat, yn.id)
 	elif "thank" in text.lower():
 		text1="It's my Pleasure 😃."
